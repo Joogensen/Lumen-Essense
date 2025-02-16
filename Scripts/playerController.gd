4169720f8ae0d2fb0ba899e956b51f5694002408
Jack Zammit <jackzammit@hotmail.com>
2025-02-16 16:22:51 -0600

    MovementWallJump

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -5,7 +5,8 @@ extends CharacterBody2D
 @export_range(0,1) var acceleration = 0.1
 @export_range(0,1) var deceleration = 0.1
 
-const JUMP_VELOCITY = -400.0
+@export var jump_force = -400
+@export_range(0,1) var decelerate_on_jump_release = 0.5
 
 
 func _physics_process(delta: float) -> void:
@@ -14,9 +15,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall()):
+		velocity.y = jump_force
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -26,3 +30,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
 
 	move_and_slide()
+	
+	
