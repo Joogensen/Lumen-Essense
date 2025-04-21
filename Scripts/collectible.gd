90d8100d3d95f2aaf2e0ed7e68e7ead2daef3987
Jack Zammit <jackzammit@hotmail.com>
2025-04-21 11:50:51 -0500

    Audio Fix

--- Scripts/collectible.gd
+++ Scripts/collectible.gd
@@ -7,8 +7,14 @@ func _ready():
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
-	if body.name == "Player":  
+	if body.name == "Player":
 		collected.emit()
 		$AudioStreamPlayer2D.play()
-		await $AudioStreamPlayer2D.finished
-		queue_free()
+		visible = false
+		call_deferred("disable_collision_and_cleanup")
+
+func disable_collision_and_cleanup():
+	monitoring = false
+	$CollisionShape2D.disabled = true
+	await get_tree().create_timer(2.0).timeout
+	queue_free()
