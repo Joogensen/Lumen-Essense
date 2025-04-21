c25ea4249685321c75ee0d2e66ccdd3c1430b994
Jack Zammit <jackzammit@hotmail.com>
2025-04-21 10:42:45 -0500

    Removed Wall Jump

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -74,7 +74,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall()):
+	if Input.is_action_just_pressed("jump") and (is_on_floor()):
 		velocity.y = jump_force
 	elif Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= decelerate_on_jump_release
