2474a07fdd12f459b26c8f86c5893e850d3e7c42
Jack Zammit <jackzammit@hotmail.com>
2025-04-27 20:43:55 -0500

    small fix

--- Scripts/simple_map.gd
+++ Scripts/simple_map.gd
@@ -6,28 +6,25 @@ var map_size
 @onready var player = $".."
 @onready var icon = $SubViewportContainer/Sprite2D
 
-
 func _ready() -> void:
 	map_size = Vector2(935, 140)
 	world_size = Vector2(6090, 1250)
 	hide()
 
-
 func _process(_delta: float) -> void:
-	if player.position.x >= 3800:
-		if player and icon:
-			var player_world_position = player.position
-			var scaled_position_x = (player_world_position.x - 3800) / world_size.x * map_size.x
-			var scaled_position_y = player_world_position.y / world_size.y * map_size.y * 1.35
-			icon.position.x = scaled_position_x + 48
-			icon.position.y = scaled_position_y - 5
-	
-		if GameState.is_player_dead:
-			GameState.map_open = false
-		elif Input.is_action_just_pressed("ui_map"):
-			GameState.map_open = !GameState.map_open
-	
-		if GameState.map_open:
-			show()
-		else:
-			hide()
+	if GameState.is_player_dead:
+		GameState.map_open = false
+	elif Input.is_action_just_pressed("ui_map"):
+		GameState.map_open = !GameState.map_open
+
+	if GameState.map_open:
+		show()
+	else:
+		hide()
+
+	if player and icon:
+		var scaled_position_x = (player.position.x) / world_size.x * map_size.x
+		var scaled_position_y = (player.position.y) / world_size.y * map_size.y * 1.35
+
+		icon.position.x = scaled_position_x
+		icon.position.y = scaled_position_y
